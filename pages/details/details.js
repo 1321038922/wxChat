81ceffb07a5906d41d2fb8940535039b86e689ca
unknown <1321038922@qq.com>
2018-09-06 21:53:19 +0800

    第四天进度

--- pages/details/details.js
+++ pages/details/details.js
@@ -1,93 +1,117 @@
 // pages/details/details.js
 
-import {fetch} from '../../utils/util.js'
+import {
+   fetch
+} from '../../utils/util.js'
 Page({
 
-  /**
-   * 页面的初始数据
-   */
-  data: {
-    bookId:"",
-    bookData:{},
-    isLoading:false
-  },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-    this.setData({
-      bookId:options.id
-    })
-    this.getDate();
-  },
-  getDate(){
-     this.setData({
-        isLoading:true
-     })
-    fetch.get(`/book/${this.data.bookId}`).then(res=>{
-      this.setData({
+   /**
+    * 页面的初始数据
+    */
+   data: {
+      bookId: "",
+      bookData: {},
       isLoading: false,
-        bookData:res
+      isCollect:""
+   },
+
+   /**
+    * 生命周期函数--监听页面加载
+    */
+   onLoad: function(options) {
+      this.setData({
+         bookId: options.id
       })
-    }).catch(err=>{
-       this.setData({
-          isLoading:false
-       })
-    })
-  },
+      this.getDate();
+   },
+   getDate() {
+      this.setData({
+         isLoading: true
+      })
+      fetch.get(`/book/${this.data.bookId}`).then(res => {
+
+         this.setData({
+            isLoading: false,
+            bookData: res
+         })
+      }).catch(err => {
+         this.setData({
+            isLoading: false
+         })
+      })
+   },
+   handleCollect(){
+      fetch.post(`/collection`,{bookId:this.data.bookId}).then(res=>{
+        if(res.code == 200){
+           wx.showToast({
+              title: '收藏成功',
+              type:'success',
+              duration:1000
+           })
+           let bookData = {...this.data.bookData}
+           bookData.isCollect = 1
+           this.setData({
+              bookData:bookData
+           })
+        }
+      })
+   },
    toCatalog(event) {
       wx.navigateTo({
          url: `/pages/catalog/catalog?id=${this.data.bookId}`,
       })
    },
 
-  /**          
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-  
-  },
+   /**          
+    * 生命周期函数--监听页面初次渲染完成
+    */
+   onReady: function() {
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-  
-  },
+   },
 
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-  
-  },
+   /**
+    * 生命周期函数--监听页面显示
+    */
+   onShow: function() {
 
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-  
-  },
+   },
 
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-  
-  },
+   /**
+    * 生命周期函数--监听页面隐藏
+    */
+   onHide: function() {
 
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-  
-  },
+   },
 
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-  
-  }
+   /**
+    * 生命周期函数--监听页面卸载
+    */
+   onUnload: function() {
+
+   },
+
+   /**
+    * 页面相关事件处理函数--监听用户下拉动作
+    */
+   onPullDownRefresh: function() {
+
+   },
+
+   /**
+    * 页面上拉触底事件的处理函数
+    */
+   onReachBottom: function() {
+
+   },
+
+   /**
+    * 用户点击右上角分享
+    */
+   onShareAppMessage: function() {
+      return {
+         title: this.data.bookData.data.title,
+         path: `/page/details/details?id=${this.data.bookId}`,
+         imageUrl: this.data.bookData.data.img
+      }
+   }
 })
--- pages/index/index.js
+++ pages/index/index.js
@@ -1,49 +1,108 @@
 //index.js
 //获取应用实例
-import {fetch} from '../../utils/util.js'
+import {
+   fetch,
+   login
+} from '../../utils/util.js'
 const app = getApp()
 Page({
-  data: {
-    swiperData:[],
-    mainContent:[],
-    indicatorDots: true,
-    autoplay: false,
-    interval: 3000,
-    duration: 500,
-    isLoading:false
-  },
-  onLoad() {
-    this.getData()
-    this.getContent()
-  },
-  getData() {
-     this.setData({
-        isLoading:true
-     })
-    fetch.get('/swiper').then(res => {
-      this.setData({
-        swiperData:res.data,
-        isLoading:false
+   data: {
+      swiperData: [],
+      mainContent: [],
+      indicatorDots: true,
+      autoplay: false,
+      interval: 3000,
+      duration: 500,
+      isLoading: false,
+      pn: 1,
+      hasMore: true,
+      loadDown: false
+   },
+   onLoad() {
+      Promise.all([this.getData(), this.getContent()]).then(() => {
+         this.setData({
+            loadDown: true,
+            pn: 1,
+            hasMore: true
+         })
       })
-    })
-  },
-  getContent(){
-    fetch.get('/category/books').then(res=>{
-       console.log(res)
-      this.setData({
-        mainContent: res.data,
-        isLoading:false,
+   },
+   getData() {
+      return new Promise((resolve, reject) => {
+         this.setData({
+            isLoading: true
+         })
+         fetch.get('/swiper').then(res => {
+            resolve()
+            this.setData({
+               swiperData: res.data,
+               isLoading: false
+            })
+         }).catch(err => {
+            reject()
+            this.setData({
+               isLoading: false
+            })
+         })
       })
-    }).catch(err=>{
-       this.setData({
-          isLoading:false
-       })
-    })
-  },
-  jumpBook(event){
-    const id = event.currentTarget.dataset.id
-    wx.navigateTo({
-      url: `/pages/details/details?id=${id}`,
-    })
-  }
+   },
+   getContent() {
+      return new Promise((resolve, reject) => {
+         fetch.get('/category/books').then(res => {
+            resolve()
+            console.log(res)
+            this.setData({
+               mainContent: res.data,
+               isLoading: false,
+            })
+         }).catch(err => {
+            reject()
+            this.setData({
+               isLoading: false
+            })
+         })
+      })
+   },
+   getMoreContent() {
+      return new Promise((resolve, reject) => {
+         fetch.get('/category/books', {
+            pn: this.data.pn
+         }).then(res => {
+            let newArr = [...this.data.mainContent, ...res.data]
+            this.setData({
+               mainContent: newArr
+            })
+            resolve(res)
+         })
+      })
+   },
+   jumpBook(event) {
+      const id = event.currentTarget.dataset.id
+      wx.navigateTo({
+         url: `/pages/details/details?id=${id}`,
+      })
+   },
+   onPullDownRefresh() {
+      Promise.all([this.getData(), this.getContent()]).then(() => {
+         wx.stopPullDownRefresh();
+         this.setData({
+            pn: 1,
+            hasMore: true
+         })
+      })
+   },
+   onReachBottom() {
+      if (this.data.hasMore) {
+         this.setData({
+            pn: this.data.pn + 1
+         })
+         this.getMoreContent().then(res => {
+            if (res.data.length < 2) {
+               this.setData({
+                  hasMore: false
+               })
+            }
+         })
+      }
+   }
 })
